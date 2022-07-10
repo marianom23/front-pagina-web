688aa4b5758a31ede4c1fdbbb11bb5b07d5e457d
Jose Pasini <jose.pasini@mercadolibre.com>
2022-07-09 23:12:39 -0300

    remove cookies in logout.

--- src/components/usuario/LogoutGoogle.jsx
+++ src/components/usuario/LogoutGoogle.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { GoogleLogout } from 'react-google-login'
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+
 export const LogoutGoogle = () => {
+  const cookies = new Cookies();
     let navigate = useNavigate();
     const clientId="664754626894-llp569b8q2er9gq0b11ib1j50529evju.apps.googleusercontent.com"
     const onSuccess = () => {
         console.log("Log out successfully")
+        cookies.remove("id", { path: '/' })
+        cookies.remove("email", { path: '/' })
+        cookies.remove("rol", { path: '/' })
         navigate("/login", { replace: true });
     }
 
