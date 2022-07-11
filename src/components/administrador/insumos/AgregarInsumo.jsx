bc473d27e9da228feb2ea0b7ae4a2d227fef5c76
Jose Pasini <jose.pasini@mercadolibre.com>
2022-07-11 00:36:05 -0300

    add select in unidad_medida

--- src/components/administrador/insumos/AgregarInsumo.jsx
+++ src/components/administrador/insumos/AgregarInsumo.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
-import { Navbar } from '../../generales/Navbar';
+import { NavbarAdministrador } from '../NavbarAdministrador';
 import { Footer } from '../../generales/Footer';
 import {
   MDBInput,
@@ -14,14 +14,39 @@ import {
 
 export const AgregarInsumo = () => {
 
+  const options = [
+    {
+      id:1,
+      label: "Litros",
+      value: "litros",
+    },
+    {
+      id:2,
+      label: "Gramos",
+      value: "gramos",
+    },
+    {
+      id:3,
+      label: "Kilogramos",
+      value: "kilogramos",
+    },
+  ];
+
+
   const [data, setData] = useState({ denominacion: "", precio_compra: "", precio_venta: "", stock_actual: "", stock_minimo: "", unidad_medida: "", es_insumo: false })
   let navigate = useNavigate();
   const handleChange = ({ target }) => {
+    console.log("Target value:", target.value)
+    console.log("Target name:", target.name)
     setData({
       ...data,
       [target.name]: target.value
     })
-
+    console.log("-----")
+    console.log("Target value:", target.value)
+    console.log("Target name:", target.name)
+    console.log("data:", data)
+    
   }
 
   const handleSubmit = async (e) => {
@@ -40,7 +65,7 @@ export const AgregarInsumo = () => {
       unidad_medida: data.unidad_medida,
       es_insumo: Boolean(data.es_insumo)
     }
-    
+
     console.log(articuloInsumo)
 
     const res = await axios.post('https://el-buen-sabor.herokuapp.com/articulo-insumo', articuloInsumo)
@@ -59,27 +84,34 @@ export const AgregarInsumo = () => {
   }
 
   return (
-    <><Navbar />
-    <div className="container">
-      <h2>Añadir insumo</h2>
-      <br />
-      <form onSubmit={handleSubmit}>
-        <MDBInput value={data.denominacion} onChange={handleChange} type='text' className='mb-4' name="denominacion" id='denominacion' label='denominacion' />
-        <MDBInput value={data.precio_compra} onChange={handleChange} type='number' className='mb-4' name="precio_compra" id='precio_compra' label='precio_compra' />
-        <MDBInput value={data.precio_venta} onChange={handleChange} name="precio_venta" className='mb-4' type='number' id='precio_venta' label='precio_venta' />
-        <MDBInput value={data.stock_actual} onChange={handleChange} name="stock_actual" className='mb-4' type='number' id='stock_actual' label='stock_actual' />
-        <MDBInput value={data.stock_minimo} onChange={handleChange} name="stock_minimo" className='mb-4' type='number' id='stock_minimo' label='stock_minimo' />
-        <MDBInput value={data.unidad_medida} onChange={handleChange} name="unidad_medida" className='mb-4' type='text' id='unidad_medida' label='unidad_medida' />
-        <MDBCheckbox value={data.es_insumo} onChange={handleChange} name='es_insumo' id='es_insumo' type='checkbox' label='Insumo' />
-        <MDBBtn type='submit' className='mb-4' block>
-          Agregar Insumo
-        </MDBBtn>
-        <MDBBtn type='submit' onClick={handleReturn} className='mb-4' block>
-          Cancelar
-        </MDBBtn>
-      </form>
-    </div>
-    <Footer />
+    <><NavbarAdministrador />
+      <div className="container">
+        <h2>Añadir insumo</h2>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <MDBInput value={data.denominacion} onChange={handleChange} type='text' className='mb-4' name="denominacion" id='denominacion' label='denominacion' />
+          <MDBInput value={data.precio_compra} onChange={handleChange} type='number' className='mb-4' name="precio_compra" id='precio_compra' label='precio_compra' />
+          <MDBInput value={data.precio_venta} onChange={handleChange} name="precio_venta" className='mb-4' type='number' id='precio_venta' label='precio_venta' />
+          <MDBInput value={data.stock_actual} onChange={handleChange} name="stock_actual" className='mb-4' type='number' id='stock_actual' label='stock_actual' />
+          <MDBInput value={data.stock_minimo} onChange={handleChange} name="stock_minimo" className='mb-4' type='number' id='stock_minimo' label='stock_minimo' />
+         
+          <label><b>unidad medida</b></label><select className="select-container" value={data.unidad_medida} onChange={handleChange} name="unidad_medida">
+            {options.map(obj =>
+              <option key={obj.id} value={obj.value} >{obj.label}</option>
+            )}
+          </select>
+
+          <MDBCheckbox value={data.es_insumo} onChange={handleChange} name='es_insumo' id='es_insumo' type='checkbox' label='Insumo' />
+
+          <MDBBtn type='submit' className='mb-4' block>
+            Agregar Insumo
+          </MDBBtn>
+          <MDBBtn type='submit' onClick={handleReturn} className='mb-4' block>
+            Cancelar
+          </MDBBtn>
+        </form>
+      </div>
+      <Footer />
     </>
   )
 }
