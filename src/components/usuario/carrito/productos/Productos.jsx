01735f99bc12c8ac22ff850679d2593382f53908
Mariano <marianomartin496@gmail.com>
2022-07-11 22:30:17 -0300

    fix deploy

--- src/components/usuario/carrito/productos/Productos.jsx
+++ src/components/usuario/carrito/productos/Productos.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
-import { MDBCard, MDBCardImage, MDBCardBody, MDBBtn, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardImage, MDBCardBody, MDBBtn, MDBCardTitle, MDBCardText, MDBRow } from 'mdb-react-ui-kit';
 import { CartContext } from '../../context/CartContext'
 import styles from "./styles.module.scss";
 
@@ -15,34 +15,30 @@ export const Productos = () => {
     }  
     const [data, setData] = useState([])
 
-
     useEffect(() => {
       getData().then((response) => {
           setData(response.data)
       })
     },[])
 
-
-
   return (    
             <MDBRow className={styles.productsContainer}>{
-                    data.map(
-                
+                    data.map(      
                         (info)=>(                 
-                                    <MDBCard key={info.id} className={styles.product}>
-                                        <MDBCardImage
-                                        src={info.imagen}
-                                        alt={info.denominacion}
-                                        position='top'
-                                        />
-                                        <MDBCardBody>
-                                        <MDBCardTitle>{info.denominacion}</MDBCardTitle>
-                                        <MDBCardText>
-                                            ${info.precio_venta} - Tiempo estimado: {info.tiempo_estimado_cocina} minutos
-                                        </MDBCardText>
-                                        </MDBCardBody>
-                                        <MDBBtn onClick={() => addItemToCart(info)}>Añadir al carrito</MDBBtn>
-                                    </MDBCard>                                             
+                            <MDBCard key={info.id} className={styles.product}>
+                                <MDBCardImage
+                                src={info.imagen}
+                                alt={info.denominacion}
+                                position='top'
+                                />
+                                <MDBCardBody>
+                                <MDBCardTitle>{info.denominacion}</MDBCardTitle>
+                                <MDBCardText>
+                                    ${info.precio_venta} - Tiempo estimado: {info.tiempo_estimado_cocina} minutos
+                                </MDBCardText>
+                                </MDBCardBody>
+                                <MDBBtn onClick={() => addItemToCart(info)}>Añadir al carrito</MDBBtn>
+                            </MDBCard>                                             
                         )
                     )    
                 }
