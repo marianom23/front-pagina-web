b57b0871cbd0491d42886f06fa95f65467b8c073
Mariano <marianomartin496@gmail.com>
2022-07-14 13:30:11 -0300

    Actualizacion fix

--- src/components/generales/google/LoginGoogle.jsx
+++ src/components/generales/google/LoginGoogle.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import jwt_decode from "jwt-decode"
+// import React, { useEffect, useState } from 'react'
+// import jwt_decode from "jwt-decode"
 
-export const LoginGoogle = () => {
-    const [user, setUser] = useState({})
+// export const LoginGoogle = () => {
+//     const [user, setUser] = useState({})
 
 
-    const handleCallbackResponse = (response) =>{
-        var userObject = jwt_decode(response.credential);
-        console.log(userObject)
-        setUser(userObject)
-        document.getElementById("signInDiv").hidden = true;
-    } 
+//     const handleCallbackResponse = (response) =>{
+//         var userObject = jwt_decode(response.credential);
+//         console.log(userObject)
+//         setUser(userObject)
+//         document.getElementById("signInDiv").hidden = true;
+//     } 
 
-    const handleSignOut = (event) => {
-        setUser({});
-        document.getElementById("signInDiv").hidden = false;
-    }
+//     const handleSignOut = (event) => {
+//         setUser({});
+//         document.getElementById("signInDiv").hidden = false;
+//     }
 
-    useEffect(() => {
-        google.accounts.id.initialize({
-            client_id: "664754626894-llp569b8q2er9gq0b11ib1j50529evju.apps.googleusercontent.com",
-            callback: handleCallbackResponse
-        })
+//     useEffect(() => {
+//         google.accounts.id.initialize({
+//             client_id: "664754626894-llp569b8q2er9gq0b11ib1j50529evju.apps.googleusercontent.com",
+//             callback: handleCallbackResponse
+//         })
             
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            {theme:"outline", size: "large"}
-        )
-    }, [])
+//         google.accounts.id.renderButton(
+//             document.getElementById("signInDiv"),
+//             {theme:"outline", size: "large"}
+//         )
+//     }, [])
 
 
 
-  return (
-    <div className="container">
-        <div id="signInDiv"></div>
+//   return (
+//     <div className="container">
+//         <div id="signInDiv"></div>
 
-        { Object.keys(user).length != 0 &&
-           <button onClick={(e) => handleSignOut(e)}> Sign Out </button>
-        }
+//         { Object.keys(user).length != 0 &&
+//            <button onClick={(e) => handleSignOut(e)}> Sign Out </button>
+//         }
 
-        {user && 
-            <div> 
-                <img src={user.picture}/>
-                <h3>{user.name}</h3>
-            </div>
-        }
-    </div>
-  )
-}
+//         {user && 
+//             <div> 
+//                 <img src={user.picture}/>
+//                 <h3>{user.name}</h3>
+//             </div>
+//         }
+//     </div>
+//   )
+// }
