be5f2d620c0ff124263431e9610cae588bed6c6d
Mariano <marianomartin496@gmail.com>
2022-07-24 00:12:32 -0300

    fix

--- src/components/detalle-pedido/DetallePedido.jsx
+++ src/components/detalle-pedido/DetallePedido.jsx
@@ -24,6 +24,12 @@ useEffect(() => {
     })
 },[])
 let total = 0;
+data.forEach(info => {
+    total += info.subtotal * info.cantidad
+});
+
+
+
 
   return (
     <>
@@ -35,8 +41,9 @@ let total = 0;
                             <img src={info.imagen} alt={info.denominacion} className="card__img" />
                             <div className="card__body">
                             <h2 className="card__title"> {info.denominacion}</h2>
+                            <p className="card__description">Cantidad: {info.cantidad}</p>
                             <p className="card__description">precio por unidad: {info.subtotal}</p>
-                            <h3 className="card__price">{total += info.subtotal * info.cantidad}total: {info.subtotal * info.cantidad}</h3>
+                            <h3 className="card__price">total: {info.subtotal * info.cantidad}</h3>
                             </div>
                         </div>      
                     )
