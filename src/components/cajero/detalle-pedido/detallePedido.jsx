2e9b4a2784b4c4ba720797709edbe81bc7b8fe2d
Mariano <marianomartin496@gmail.com>
2022-07-23 21:23:06 -0300

    fix

---
+++ src/components/cajero/detalle-pedido/detallePedido.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const detallePedido = ({info}) => {
+  return (
+    <div>detallePedido</div>
+  )
+}
---
+++ src/components/cocinero/PedidosCocina.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import './productos.css'
+
+
+export const PedidosCocina = () => {
+
+    const [data, setData] = useState([])
+
+    const getData = async () => {
+    const response = await axios.get('https://el-buen-sabor.herokuapp.com/pedido/getAll')
+    console.log(response)
+    return response
+    }   
+
+    const cambiarEstado =  async (numero, id) => {
+    const pedido = {
+        "id_pedido":id,
+        "estado":numero
+    }
+    const response = await axios.put('https://el-buen-sabor.herokuapp.com/pedido/update-estado', pedido)
+    console.log(response)
+    }   
+
+    useEffect(() => {
+    getData().then((response) => {
+    setData(response.data)
+    })
+    },[])
+
+    const verDetalle = (id) => {
+
+    }
+
+
+
+  return (
+    <div className='wrapper'>
+        {         
+            data.map(      
+                (info)=>(          
+                    (info.estado === 2 ? 
+                        <div className="card" key={info.id}>
+                            <img src="assets/pedidoPendiente.png" alt={info.denominacion} className="card__img" />
+                            <div className="card__body">
+                            <h2 className="card__title">Pedido {info.id}</h2>
+                            <p className="card__description">{info.tiempo_estimado_cocina} minutos</p>
+                            <h3 className="card__price">{info.precio_venta}</h3>
+                            <button className="card__btn" onClick={() => verDetalle(info.id)}>Ver detalle</button>
+                            <button className="card__btn" onClick={() => cambiarEstado(3,info.id)}>Terminar</button>
+                            </div>
+                        </div>      
+                    :
+                        ""
+                    )
+
+                )
+            )
+        }
+    </div>
+  )
+}
